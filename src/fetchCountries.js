5ede18e3e24ec46256ee6c920ed7d85632d28411
Illya Pavlenko <illiampavlenko@gmail.com>
2022-11-10 15:48:14 +0200

    recode task. complete

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,18 +1,14 @@
-const filterOptions = '?fields=name,capital,population,flags,languages';
+const BASE_URL = 'https://restcountries.com/v3.1/name';
 
-function fetchCountries(name) {
-    const url = `https://restcountries.com/v3.1/name/${name}`;
-
-    return fetch(url + filterOptions)
-        .then(response => {
-            if(!response.ok) {
-                throw new Error(response.status);
-            }
-            return response.json();
-        })
-        .then(countries => {
-            return countries;
-        });
-}
+const fetchCountries = name => {
+  return fetch(
+    `${BASE_URL}/${name}?fields=name,capital,population,flags,languages`
+  ).then(response => {
+    if (response.status === 404) {
+      return Promise.reject(new Error());
+    }
+    return response.json();
+  });
+};
 
 export { fetchCountries };
--- src/index.js
+++ src/index.js
@@ -1,90 +1,93 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
-
 import { fetchCountries } from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
 
 const refs = {
-    input: document.querySelector('#search-box'),
-    countryList: document.querySelector('.country-list'),
-    countryInfo: document.querySelector('.country-info'),
+  input: document.querySelector('#search-box'),
+  countryList: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.country-info'),
+};
+
+let country = '';
+
+refs.input.addEventListener('input', debounce(searchInput, DEBOUNCE_DELAY));
+
+function clearMarkup() {
+  refs.countryList.innerHTML = '';
+  refs.countryInfo.innerHTML = '';
 }
 
-refs.input.addEventListener('input', debounce(searchCountry, DEBOUNCE_DELAY));
-
-function clearInput() {
-    refs.countryList.innerHTML = '';
-    refs.countryInfo.innerHTML = '';
+function searchInput(e) {
+  clearMarkup();
+  country = e.target.value.trim();
+  fetchCountries(country).then(createMarkup).catch(onError);
 }
 
-function searchCountry() {
-    const searchInput = String(refs.input.value);
-    clearInput();
-
-    if (searchInput.trim()) {
-        fetchCountries(searchInput)
-            .then(countries => {
-                if (countries.length > 10) {
-                    Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
-                } else if (countries.length >= 2 && countries.length < 10) {
-                    renderCountries(countries);
-                } else {
-                    renderInfo(countries);
-                }
-            }
-        ).catch(error => {
-            Notiflix.Notify.failure('Oops, there is no country with that name');
-        });
-    }
+function onError(error) {
+  // if (error.message === '404') {
+    return Notiflix.Notify.failure('Oops, there is no country with that name');
+  // }
+  // return Notiflix.Notify.failure(`Oops, server error:"${error}"`);
 }
 
-function renderCountries(countries = []) {
-    clearInput();
-
-    const markupContries = countries
-    .map(({flag: {svg}, name: {official}}) => {
-        return `
-            <li class="country-list__item>
-            <img class="country-list__icon" width="20px" src="${country.flag.svg}" alt="${country.name.official}">
-            <p class="country-list__name">${country.name.official}</p>
-            </li>    
-        `;
-    }).join('');
-    refs.countryList.innerHTML = markupContries;
+function createMarkup(data) {
+  if (data.length > 10) {
+    clearMarkup();
+    Notiflix.Notify.info(
+      'Too many matches found. Please enter a more specific name.'
+    );
+  } else if (data.length >= 2 && data.length <= 10) {
+    markupCountiesList(data);
+  } else {
+    markupCountryInfo(data);
+  } 
 }
 
-function renderInfo(countries = []) {
-    clearInput();
-    const markupContries = countries
-      .map(country => {
-        return `
-          <div class="country-info__item">
-            <img class="country-list__icon" src="${country.flags.svg}" alt="${
-          country.name.official
-        }" width="40px" height="30px" />
-            <h2 class="country-info__title">${country.name.official}</h2>
-          </div>
-          <div class="article-wrapper">
-            <p class="country-info__article">
-              Capital:
-            </p><span class="country-info__span">${country.capital}</span>
-          </div>
-          <div class="article-wrapper">
-            <p class="country-info__article">
-              Population:
-            </p><span class="country-info__span">${country.population}</span>
-          </div>
-          <div class="article-wrapper">
-            <p class="country-info__article">
-              Languages:
-            </p><span class="country-info__span">${Object.values(
-              country.languages
-            ).join(',')}</span>
-          </div>
-        `;
-      })
-      .join('');
-    refs.countryInfo.innerHTML = markupContries;
-  }
+function markupCountiesList(data) {
+  clearMarkup();
+  const markupList = data
+    .map(country => {
+      return `<li class="country-list__item">
+            <img class="country-list__icon"src=${country.flag.svg} width=40>
+            <h2 class="country-list__name">${country.name.official}</h2>
+            </li>`;
+    })
+    .join('');
+
+  refs.countryList.insertAdjacentHTML('afterbegin', markupList);
+}
+
+function markupCountryInfo(data) {
+  clearMarkup();
+  const markupInfo = data
+    .map(country => {
+      return `<div class="country-info__item">
+                <img class="country-list__icon" src="${
+                  country.flags.svg
+                }" alt="${country.name.official}" width="40px" height="30px" />
+                <h2 class="country-info__title">${country.name.official}</h2>
+              </div>
+              <div class="article-wrapper">
+                <p class="country-info__article">Capital:</p><span class="country-info__span">${
+                  country.capital
+                }</span>
+              </div>
+              <div class="article-wrapper">
+                <p class="country-info__article">Population:</p><span class="country-info__span">${
+                  country.population
+                }</span>
+              </div>
+              <div class="article-wrapper">
+                <p class="country-info__article">Languages:</p><span class="country-info__span">${Object.values(
+                  country.languages
+                ).join(',')}</span>
+              </div>
+            `;
+    })
+    .join('');
+
+  refs.countryInfo.insertAdjacentHTML('afterbegin', markupInfo);
+}
